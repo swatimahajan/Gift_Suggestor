b403d13c1d91efab9629b7f4a35567240ee854f5
swatimahajan <mahajanswati108@gmail.com>
2015-11-06 17:22:58 +0530

    modified files

--- suggestor/views.py
+++ suggestor/views.py
@@ -4,3 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request , 'suggestor/index.html')
+    #return HttpResponse(template.render(context))
